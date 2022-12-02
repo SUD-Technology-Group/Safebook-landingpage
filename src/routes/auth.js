e120618235ef45267034d79aee478b0b8cf51bf5
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2022-12-03 03:43:05 +0700

    update frontend eps 1

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,15 +5,11 @@ const { authController } = require('../controllers');
 
 // Current path: /auth
 router.get('/', (req, res) => {
-	res.redirect('auth/login');
+  return res.render('client/index');
 });
 router.get('/login', authController.loginView);
 router.post('/login', authController.login);
-router.post(
-	'/change-password',
-	authController.changePassword,
-	authController.logout
-);
+router.post('/change-password', authController.changePassword, authController.logout);
 router.get('/logout', authController.logout);
 
 module.exports = router;
