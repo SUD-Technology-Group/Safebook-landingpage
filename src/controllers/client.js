0f9570cc9288ec07ae7249bda49c5e7eff7a4bdc
vinhnguyen-22 <vinhnguyenad22@gmail.com>
2022-12-04 05:39:02 +0700

    Fix index

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -1,30 +1,25 @@
 const catchAsync = require('../utils/catchAsync');
 const customers = require('../../mock/customer');
 
-const {
-    sectionService,
-    bannerService,
-    commentService,
-    staffService,
-} = require('../services');
+const { sectionService, bannerService, commentService, staffService } = require('../services');
 
 const clientController = {
-    // GET /
-    index: catchAsync(async (req, res) => {
-        const sections = await sectionService.get({});
-        const banners = await bannerService.get({});
-        const comments = await commentService.get({});
-        const staffs = await staffService.get({});
-
-        res.render('client', {
-            title: 'Trang chủ',
-            sections,
-            customers,
-            comments,
-            banners,
-            staffs,
-        });
-    }),
+  // GET /
+  index: catchAsync(async (req, res) => {
+    const sections = await sectionService.get({});
+    const banners = await bannerService.get({});
+    const comments = await commentService.get({});
+    const staffs = await staffService.get({});
+    console.log(banners);
+    res.render('client', {
+      title: 'Trang chủ',
+      sections,
+      customers,
+      comments,
+      banners,
+      staffs,
+    });
+  }),
 };
 
 module.exports = clientController;
